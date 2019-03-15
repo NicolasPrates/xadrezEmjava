58b20094d1ff01eb30900d5713eaef5dd50cd438
Nicolas Prates <nicolaspratesaraujo@gmail.com>
2019-03-15 20:22:39 -0300

    First Class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
-
+		Position teste = new Position(1,2);
+		System.out.println(teste.toString());
 	}
 
 }
